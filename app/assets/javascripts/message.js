0e29c8c0e0e31bee3d0a3309c670b531c7d9088b
matsushii <mtsmath24@yahoo.co.jp>
2019-10-08 14:19:36 +0900

    送信時のイベント発火およびajaxからのrquest確認
    送信時にイベント発火が行われることを確認後、ajaxによりコントローラへ送られているリクエストの内容をbinding-pryにより確認した。その際、gem pry-railsをinstallした。

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var message = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({  
+      url: url,
+      type: "POST",
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
+})
