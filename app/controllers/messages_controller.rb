3230dfd69db98ad26529b1f5f1926246adaa2b2f
matsushii <mtsmath24@yahoo.co.jp>
2019-10-09 12:36:20 +0900

    コメントアウトの削除
    不要なコメントアウトを削除した。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,18 @@
 class MessagesController < ApplicationController
-  before_action :set_group  #全てのメソッドの前にset_groupメソッドを実行する
+  before_action :set_group 
 
 
   def index
-    @message = Message.new  #messageインスタンス変数の定義
+    @message = Message.new  
     @messages = @group.messages.includes(:user) 
-    #@groupのmessagesをuser情報を結びつけて@messagesへ代入
+
   end
 
   def create
-    @message = @group.messages.new(message_params)  #messageインスタンス変数の定義
-    if @message.save  #messageが保存された場合
+    @message = @group.messages.new(message_params)
+    if @message.save
       respond_to do |format|
-        format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'} #再度そのグループへアクセスしてnoticeの表示
+        format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
         format.json
       end
     else
@@ -23,11 +23,11 @@ class MessagesController < ApplicationController
 
   private
 
-  def message_params #content,imageを含むparamsをrequierで取得してmergeでcurrent_user.idと結合して引数として渡す
+  def message_params 
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
-  def set_group #Groupテーブルからgroup_idをキーとしてレコードをとってきて@groupに渡す
+  def set_group
     @group = Group.find(params[:group_id])
   end
 
