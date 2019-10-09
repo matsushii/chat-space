7cdd351be26b8a6d7974311fff8e5014555ce89e
matsushii <mtsmath24@yahoo.co.jp>
2019-10-09 12:26:30 +0900

    controllerをjson対応に編集
    controllerへjsonが渡された際の処理をrespond_toにて振り分けるようにした。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,30 +1,33 @@
 class MessagesController < ApplicationController
-  before_action :set_group
+  before_action :set_group  #全てのメソッドの前にset_groupメソッドを実行する
+
 
   def index
-    @message = Message.new
-    @messages = @group.messages.includes(:user)
+    @message = Message.new  #messageインスタンス変数の定義
+    @messages = @group.messages.includes(:user) 
+    #@groupのmessagesをuser情報を結びつけて@messagesへ代入
   end
 
   def create
-    binding.pry
-    @message = @group.messages.new(message_params)
-    if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    @message = @group.messages.new(message_params)  #messageインスタンス変数の定義
+    if @message.save  #messageが保存された場合
+      respond_to do |format|
+        format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'} #再度そのグループへアクセスしてnoticeの表示
+        format.json
+      end
     else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
-      render :index
+        @messages = @group.message.includes(:user)
+        flash.now[:alert] = 'メッセージを入力してください。'
     end
   end
 
   private
 
-  def message_params
+  def message_params #content,imageを含むparamsをrequierで取得してmergeでcurrent_user.idと結合して引数として渡す
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
-  def set_group
+  def set_group #Groupテーブルからgroup_idをキーとしてレコードをとってきて@groupに渡す
     @group = Group.find(params[:group_id])
   end
 
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.id    @message.id
+json.user_name    @message.user.name
+json.content    @message.content
+json.time    @message.created_at
+# json.KEY VALUEの順
