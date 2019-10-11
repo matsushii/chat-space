2f5963570a7f5ea952239510e17a7beab3422183
matsushii <mtsmath24@yahoo.co.jp>
2019-10-11 21:15:47 +0900

    編集画面での既存ユーザーの表示
    編集画面でユーザーへ遷移した際に、チャットメンバーがリセットされてしまう現象を解消した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   def index
     @users = User.where('name LIKE(?) and id != ?', "#{params[:keyword]}%",current_user).limit(10)
     respond_to do |format|
-      format.html
       format.json
     end
   end
