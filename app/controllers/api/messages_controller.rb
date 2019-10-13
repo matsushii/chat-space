1c92a3ea17d0412f5396126c1661b4e9087d6dd7
matsushii <mtsmath24@yahoo.co.jp>
2019-10-13 16:01:24 +0900

    コメントアウトの削除
    コメントアウトを削除しました。
    すみません。

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,10 +1,7 @@
 class Api::MessagesController < ApplicationController
   def index
     group = Group.find(params[:group_id])
-    #現在のgroup_idからgroupを取得し、groupへ代入
     last_message_id = params[:id].to_i
-    #messagesの最後のidを代入
     @messages = group.messages.includes(:user).where("id > #{last_message_id}")
-    #最後のidよりもidの大きいもののみを取得
   end
 end
