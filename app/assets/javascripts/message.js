dc89a701de6a6ac1c745c11bf076c3ba639f71cc
matsushii <mtsmath24@yahoo.co.jp>
2019-10-13 15:32:11 +0900

    [WIP]自動更新機能の実装
    # What
    自動更新機能の付加
    
    # Why
    current_userが更新時には非同期通信が行われるが、チャットでは他者のmessageをリアルタイムで閲覧できる必要があるため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,24 +1,35 @@
 $(function(){
   function buildHTML(message){
-    var html = `<div class="message">
+    var insertImage = '';
+    if (message.image.url) {
+        insertImage = `<img src="${message.image.url}">`;
+    }
+    var html = `<div class="message" data-id = "${message.id}">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
-                        ${ message.user_name }
+                      ${message.user_name}
                     </div>
                     <div class="upper-message__date">
-                        ${ message.time }
+                      ${message.created_at}
                     </div>
                   </div>
                   <div class="lower-message">
-                      <p class="lower-message__content">
-                          ${ message.content }
-                      </p>
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>
+                    ${insertImage}
                   </div>
                 </div>`;
     return html;
   }
+
+  function ScrollToNewMessage(){
+    $('.messages').animate({ scrollTop: $(".messages")[0].scrollHeight }, 500);
+  }
+
+
   $('#new_message').on('submit', function(e){
-    e.preventDefault();
+    e.preventDefault(); 
     var formdata = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({  
@@ -30,16 +41,43 @@ $(function(){
       contentType: false
     })
     .done(function (data) {
+      console.log(data);
       var html = buildHTML(data);
       $('.messages').append(html);
+      ScrollToNewMessage();
       $("form")[0].reset();
       $(".form__submit").prop('disabled', false);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .fail(function(data){
       alert('エラーが発生したためメッセージは送信できませんでした。');
-      $("form")[0].reset();
-      $(".form__submit").prop('disabled', false);
-    })
-  })
-})
+    });
+  });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message').last().data('id')
+    console.log(last_message_id);
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      $.ajax({
+        url: "api/messages",
+        type: 'GET',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function (messages) {
+        var insertHTML = '';
+        console.log(messages);
+        messages.forEach(function(message) {
+          insertHTML += buildHTML(message); 
+        });
+        $('.messages').append(insertHTML);
+        ScrollToNewMessage();                  
+      })
+      .fail(function() {  
+        console.log('error');
+      })
+    }
+  }
+  setInterval(reloadMessages, 5000);
+});
+
+
--- app/assets/javascripts/messages.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-  })
-})
